45d75d265d80d0a3e7d7469da51b04fcfebac12b
M4teoo <mateosolino1@gmail.com>
2024-07-28 19:56:06 +0200

    Añadido select y más directos

--- js/script.js
+++ js/script.js
@@ -1,21 +1,69 @@
-const videoUrls = [ 
-    "https://www.youtube.com/embed/mhJRzQsLZGg?si=8BPKYoEe7C7L_jYX",
-    "https://www.youtube.com/embed/cOmmvhDQ2HM?si=sxVgPt6NgoFhO_78",
-    "https://www.youtube.com/embed/Jm8wRjD3xVA?si=Pdu_UE9vCTLT6Weq",
-    "https://www.youtube.com/embed/c212qMUTnEs?si=Ny45gTYrlJUn7PYw",
-    "https://www.youtube.com/embed/uH1W01t_lGc?si=8vw_TLcuj2ojPLyn",
-    "https://www.youtube.com/embed/RsIG9WF-B4s?si=dxcCrThD29IG0faG",
-    "https://www.youtube.com/embed/qw3uaLRrYNY?si=LO7Aj-vkxprk2Wxk",
-    "https://www.youtube.com/embed/OWCh3wmKtak?si=-kNVydJhfVFLSw5P",
-    "https://www.youtube.com/embed/Rg7kw-KLDL8?si=feM0Z4RSwj1tWsig",
-    "https://www.youtube.com/embed/IQV0DlS1Ew0?si=mHa3ojQzZIC3LPP0",
-    "https://www.youtube.com/embed/tS2PHJmvJzo?si=iQ6LSnNTii6xvG7C",
-    "https://www.youtube.com/embed/FTw5xuq2swo?si=74Iod0bXsfevK9JC",
-    // Agrega más URLs según se necesite
-];
+// const videoUrls = [ 
+    // "https://www.youtube.com/embed/mhJRzQsLZGg?si=8BPKYoEe7C7L_jYX",
+    // "https://www.youtube.com/embed/cOmmvhDQ2HM?si=sxVgPt6NgoFhO_78",
+    // "https://www.youtube.com/embed/Jm8wRjD3xVA?si=Pdu_UE9vCTLT6Weq",
+    // "https://www.youtube.com/embed/c212qMUTnEs?si=Ny45gTYrlJUn7PYw",
+    // "https://www.youtube.com/embed/uH1W01t_lGc?si=8vw_TLcuj2ojPLyn",
+    // "https://www.youtube.com/embed/RsIG9WF-B4s?si=dxcCrThD29IG0faG",
+    // "https://www.youtube.com/embed/qw3uaLRrYNY?si=LO7Aj-vkxprk2Wxk",
+    // "https://www.youtube.com/embed/OWCh3wmKtak?si=-kNVydJhfVFLSw5P",
+    // "https://www.youtube.com/embed/Rg7kw-KLDL8?si=feM0Z4RSwj1tWsig",
+    // "https://www.youtube.com/embed/IQV0DlS1Ew0?si=mHa3ojQzZIC3LPP0",
+    // "https://www.youtube.com/embed/tS2PHJmvJzo?si=iQ6LSnNTii6xvG7C",
+    // "https://www.youtube.com/embed/FTw5xuq2swo?si=74Iod0bXsfevK9JC",
+//     // Agrega más URLs según se necesite
+// ];
+
+// "https://www.youtube.com/embed/E1uVzfQG_Yo?si=BwgeZKJIlsyaeM05" // Launch Pad Starbase SFNow
+
+const cameraConfigs = {
+    config1: [
+        "https://www.youtube.com/embed/mhJRzQsLZGg?si=8BPKYoEe7C7L_jYX", // Starbase NSF
+        "https://www.youtube.com/embed/cOmmvhDQ2HM?si=sxVgPt6NgoFhO_78", // McGregor NSF
+        "https://www.youtube.com/embed/Jm8wRjD3xVA?si=Pdu_UE9vCTLT6Weq", // Space Coast NSF
+        "https://www.youtube.com/embed/c212qMUTnEs?si=Ny45gTYrlJUn7PYw", // Nerdle Cam Starbase LabPadre
+        "https://www.youtube.com/embed/uH1W01t_lGc?si=8vw_TLcuj2ojPLyn", // Cape Cam KSC LabPadre
+        "https://www.youtube.com/embed/RsIG9WF-B4s?si=dxcCrThD29IG0faG", // Sapphire Cam Starbase Labpadre
+        "https://www.youtube.com/embed/qw3uaLRrYNY?si=LO7Aj-vkxprk2Wxk", // Rocket Ranch Cam Starbase LabPadre
+        "https://www.youtube.com/embed/OWCh3wmKtak?si=-kNVydJhfVFLSw5P", // LabCam Starbase LabPadre
+        "https://www.youtube.com/embed/Rg7kw-KLDL8?si=feM0Z4RSwj1tWsig", // Rover Cam Starbase LabPadre
+        "https://www.youtube.com/embed/IQV0DlS1Ew0?si=mHa3ojQzZIC3LPP0", // Sentinel Cam Starbase LabPadre
+        "https://www.youtube.com/embed/tS2PHJmvJzo?si=iQ6LSnNTii6xvG7C", // Rover 2.0 Cam Starbase LabPadre
+        "https://www.youtube.com/embed/FTw5xuq2swo?si=74Iod0bXsfevK9JC", // SpaceX port Cape Canaveral LabPadre
+    ],
+    config2: [
+        "https://www.youtube.com/embed/mhJRzQsLZGg?si=8BPKYoEe7C7L_jYX", // Starbase NSF
+        "https://www.youtube.com/embed/c212qMUTnEs?si=Ny45gTYrlJUn7PYw", // Nerdle Cam Starbase LabPadre
+        "https://www.youtube.com/embed/RsIG9WF-B4s?si=dxcCrThD29IG0faG", // Sapphire Cam Starbase Labpadre
+        "https://www.youtube.com/embed/qw3uaLRrYNY?si=LO7Aj-vkxprk2Wxk", // Rocket Ranch Cam Starbase LabPadre
+        "https://www.youtube.com/embed/OWCh3wmKtak?si=-kNVydJhfVFLSw5P", // LabCam Starbase LabPadre
+        "https://www.youtube.com/embed/Rg7kw-KLDL8?si=feM0Z4RSwj1tWsig", // Rover Cam Starbase LabPadre
+        "https://www.youtube.com/embed/IQV0DlS1Ew0?si=mHa3ojQzZIC3LPP0", // Sentinel Cam Starbase LabPadre
+        "https://www.youtube.com/embed/tS2PHJmvJzo?si=iQ6LSnNTii6xvG7C", // Rover 2.0 Cam Starbase LabPadre
+        "https://www.youtube.com/embed/M_hvfSFAKZw?si=haBJ0aGu2HAijsKG", // Build Cam Starbase The Launch Pad
+        "https://www.youtube.com/embed/9b7Nh0iTGT4?si=8ksQYk0QWWRw4P3G", // Starbase Live Rocket Ranch
+        "https://www.youtube.com/embed/U3ERnA7MJuI?si=epu1AzNlF7JMoYSM", // SpaceX Starbase What abaout it?
+        "https://www.youtube.com/embed/E1uVzfQG_Yo?si=BwgeZKJIlsyaeM05", // Launch Pad Starbase SFNow
+    ],
+    config3: [
+        "https://www.youtube.com/embed/cOmmvhDQ2HM?si=sxVgPt6NgoFhO_78", // McGregor NSF
+        "https://www.youtube.com/embed/Jm8wRjD3xVA?si=Pdu_UE9vCTLT6Weq", // Space Coast NSF
+        "https://www.youtube.com/embed/uH1W01t_lGc?si=8vw_TLcuj2ojPLyn", // Cape Cam KSC LabPadre
+        "https://www.youtube.com/embed/FTw5xuq2swo?si=74Iod0bXsfevK9JC", // SpaceX port Cape Canaveral LabPadre
+        "https://www.youtube.com/embed/GN_j_kVsbpI?si=4cJiMh-sjA3RoRGZ", // James Web Live Data
+        "https://www.youtube.com/embed/hEqewMt1Atc?si=VpHV_BFIrr4gCbDt", // Starman Live Data
+        "https://www.youtube.com/embed/bQxEcdz_g8g?si=hLafO12LGZCDbHua", // Betelgeuse Live
+        "https://www.youtube.com/embed/xRPjKQtRXR8?si=68MC_3YOs0Q5oI4S", // ISS Live Stream afarTV
+        "https://www.youtube.com/embed/VdFK-xs_r-4?si=iZ9kjNz4VLUqsRi-", // NASA ISS LIVE
+        "https://www.youtube.com/embed/P9C25Un7xaM?si=2m_aTBSqnyswYbXS", // NASA ISS VIEWS
+        "https://www.youtube.com/embed/nA9UZF-SZoQ?si=D8NE3lVW-hupL2mp", // NASA TV'S MEDIA
+        "https://www.youtube.com/embed/21X5lGlDOfg?si=XuRkbkEsyU9R1iXG", // NASA TV
+    ]
+};
 
 function createIframes(videoUrls) {
     const videoGrid = document.getElementById('videoGrid');
+    videoGrid.innerHTML = '';  // Limpiar el grid antes de añadir nuevos iframes
     videoUrls.forEach((url, index) => {
         const iframeContainer = document.createElement('div');
         iframeContainer.id = 'player' + index;
@@ -24,9 +72,16 @@ function createIframes(videoUrls) {
     });
 }
 
-createIframes(videoUrls);
-
 function onYouTubeIframeAPIReady() {
+    initCameraConfig('config1');
+    document.getElementById('cameraConfig').addEventListener('change', (event) => {
+        initCameraConfig(event.target.value);
+    });
+}
+
+function initCameraConfig(config) {
+    const videoUrls = cameraConfigs[config];
+    createIframes(videoUrls);
     videoUrls.forEach((url, index) => {
         new YT.Player('player' + index, {
             height: '100%',
@@ -38,6 +93,7 @@ function onYouTubeIframeAPIReady() {
             }
         });
     });
+    updateGridLayout(videoUrls.length);
 }
 
 function extractVideoID(url) {
@@ -45,15 +101,19 @@ function extractVideoID(url) {
     return urlObj.searchParams.get('v') || urlObj.pathname.split('/').pop();
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+function updateGridLayout(numVideos) {
     const videoGrid = document.getElementById('videoGrid');
-
-    const numVideos = videoUrls.length;
     const numCols = Math.ceil(Math.sqrt(numVideos));
     const numRows = Math.ceil(numVideos / numCols);
 
     videoGrid.style.gridTemplateColumns = `repeat(${numCols}, 1fr)`;
     videoGrid.style.gridTemplateRows = `repeat(${numRows}, 1fr)`;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const videoGrid = document.getElementById('videoGrid');
+
+    document.getElementById('cameraConfig').value = 'config1';
 
     // Modal functionality
     const modal = document.getElementById("infoModal");
