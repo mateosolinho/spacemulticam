484b097d2d23dc96197b028ad523a55a8296206e
Mateo Soliño Blanco <mateosolino1@gmail.com>
2025-03-23 00:58:01 +0100

    Fix Lives

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 const cameraConfigs = {
+  // config1: [
+  //   "https://www.youtube.com/embed/mhJRzQsLZGg?si=8BPKYoEe7C7L_jYX", // Starbase NSF
+  //   "https://www.youtube.com/embed/cOmmvhDQ2HM?si=sxVgPt6NgoFhO_78", // McGregor NSF
+  //   "https://www.youtube.com/embed/Jm8wRjD3xVA?si=Pdu_UE9vCTLT6Weq", // Space Coast NSF
+  //   "https://www.youtube.com/embed/tFA8jo0Ejf8?si=LwYEzorIuGzV7YIz", // Nerdle Cam Starbase LabPadre
+  //   "https://www.youtube.com/embed/uH1W01t_lGc?si=8vw_TLcuj2ojPLyn", // Cape Cam KSC LabPadre
+  //   "https://www.youtube.com/embed/lDdHE8XkU34?si=4gKlmvQr2Gdc2ZE3", // Starbase The Launch Pad
+  //   "https://www.youtube.com/embed/tS2PHJmvJzo?si=iQ6LSnNTii6xvG7C", // Rover 2.0 Cam Starbase LabPadre
+  //   "https://www.youtube.com/embed/qw3uaLRrYNY?si=LO7Aj-vkxprk2Wxk", // Rocket Ranch Cam Starbase LabPadre
+  //   "https://www.youtube.com/embed/2TYo1btp740?si=dLSQ0DbQX4Q8-TV2", // ISS Live The Launch Pad
+  //   "https://www.youtube.com/embed/hAQ0rWK5j-U?si=DjoAkRN9UecadwOI", // Plex Cam Starbase LabPadre
+  //   "https://www.youtube.com/embed/IQV0DlS1Ew0?si=mHa3ojQzZIC3LPP0", // Sentinel Cam Starbase LabPadre
+  //   "https://www.youtube.com/embed/FTw5xuq2swo?si=74Iod0bXsfevK9JC", // SpaceX port Cape Canaveral LabPadre
+  // ],
   config1: [
     "https://www.youtube.com/embed/mhJRzQsLZGg?si=8BPKYoEe7C7L_jYX", // Starbase NSF
-    "https://www.youtube.com/embed/cOmmvhDQ2HM?si=sxVgPt6NgoFhO_78", // McGregor NSF
-    "https://www.youtube.com/embed/Jm8wRjD3xVA?si=Pdu_UE9vCTLT6Weq", // Space Coast NSF
-    "https://www.youtube.com/embed/c212qMUTnEs?si=Ny45gTYrlJUn7PYw", // Nerdle Cam Starbase LabPadre
-    "https://www.youtube.com/embed/uH1W01t_lGc?si=8vw_TLcuj2ojPLyn", // Cape Cam KSC LabPadre
-    "https://www.youtube.com/embed/LP3m2L_hkA4?si=DA3gdhk7g8wDzWzu", // Sapphire Cam Starbase Labpadre
-    "https://www.youtube.com/embed/tS2PHJmvJzo?si=iQ6LSnNTii6xvG7C", // Rover 2.0 Cam Starbase LabPadre
+    "https://www.youtube.com/embed/tFA8jo0Ejf8?si=LwYEzorIuGzV7YIz", // Nerdle Cam Starbase LabPadre
     "https://www.youtube.com/embed/qw3uaLRrYNY?si=LO7Aj-vkxprk2Wxk", // Rocket Ranch Cam Starbase LabPadre
-    "https://www.youtube.com/embed/YNu0ygC0njE?si=9wlbJhqy0vtpgPKc", // LabCam Starbase LabPadre
-    "https://www.youtube.com/embed/EiKm0TgbVdg?si=KoFi3s9QP9NTkXB_", // Plex Cam Starbase LabPadre
-    "https://www.youtube.com/embed/IQV0DlS1Ew0?si=mHa3ojQzZIC3LPP0", // Sentinel Cam Starbase LabPadre
-    "https://www.youtube.com/embed/FTw5xuq2swo?si=74Iod0bXsfevK9JC", // SpaceX port Cape Canaveral LabPadre
-  ],
-  config2: [
-    "https://www.youtube.com/embed/mhJRzQsLZGg?si=8BPKYoEe7C7L_jYX", // Starbase NSF
-    "https://www.youtube.com/embed/c212qMUTnEs?si=Ny45gTYrlJUn7PYw", // Nerdle Cam Starbase LabPadre
-    "https://www.youtube.com/embed/qw3uaLRrYNY?si=LO7Aj-vkxprk2Wxk", // Rocket Ranch Cam Starbase LabPadre
-    "https://www.youtube.com/embed/YNu0ygC0njE?si=9wlbJhqy0vtpgPKc", // LabCam Starbase LabPadre
-    "https://www.youtube.com/embed/EiKm0TgbVdg?si=KoFi3s9QP9NTkXB_", // Plex Cam Starbase LabPadre
-    "https://www.youtube.com/embed/LP3m2L_hkA4?si=DA3gdhk7g8wDzWzu", // Sapphire Cam Starbase Labpadre
+    "https://www.youtube.com/embed/lDdHE8XkU34?si=4gKlmvQr2Gdc2ZE3", // Starbase The Launch Pad
+    "https://www.youtube.com/embed/hAQ0rWK5j-U?si=DjoAkRN9UecadwO",  // Plex Cam Starbase LabPadre
+    "https://www.youtube.com/embed/DpCNkxtZT30?si=cLYpC-F0GM7SxhBA", // Rover Cam Labpadre
     "https://www.youtube.com/embed/IQV0DlS1Ew0?si=mHa3ojQzZIC3LPP0", // Sentinel Cam Starbase LabPadre
     "https://www.youtube.com/embed/tS2PHJmvJzo?si=iQ6LSnNTii6xvG7C", // Rover 2.0 Cam Starbase LabPadre
     "https://www.youtube.com/embed/U3ERnA7MJuI?si=epu1AzNlF7JMoYSM", // SpaceX Starbase What abaout it?
-    "https://www.youtube.com/embed/E1uVzfQG_Yo?si=BwgeZKJIlsyaeM05", // Launch Pad Starbase SFNow
-    "https://www.youtube.com/embed/EiKm0TgbVdg?si=jz4qITWOJ6jlW8Ux", // Plex View LabPadre
+    "https://www.youtube.com/embed/FTw5xuq2swo?si=wHO9ncnhNCsNXgRw", // Gator Cam
+    "https://www.youtube.com/embed/H7SvvCDNb_s?si=n5qPjAffA613jd7r", // Shappire Cam Starbase LabPadre
     
     // Cambiar
     "https://www.youtube.com/embed/cOmmvhDQ2HM?si=sxVgPt6NgoFhO_78", // McGregor NSF
@@ -32,20 +32,20 @@ const cameraConfigs = {
     // No funcionan
     // "https://www.youtube.com/embed/9b7Nh0iTGT4?si=8ksQYk0QWWRw4P3G", // Starbase Live Rocket Ranch
   ],
-  config3: [
-    "https://www.youtube.com/embed/cOmmvhDQ2HM?si=sxVgPt6NgoFhO_78", // McGregor NSF
-    "https://www.youtube.com/embed/Jm8wRjD3xVA?si=Pdu_UE9vCTLT6Weq", // Space Coast NSF
-    "https://www.youtube.com/embed/uH1W01t_lGc?si=8vw_TLcuj2ojPLyn", // Cape Cam KSC LabPadre
-    "https://www.youtube.com/embed/FTw5xuq2swo?si=74Iod0bXsfevK9JC", // SpaceX port Cape Canaveral LabPadre
-    "https://www.youtube.com/embed/GN_j_kVsbpI?si=4cJiMh-sjA3RoRGZ", // James Web Live Data
-    "https://www.youtube.com/embed/gMLwtXCfo_w?si=li_JcfcEw13MlUIr", // Starman Live Data
-    "https://www.youtube.com/embed/bQxEcdz_g8g?si=hLafO12LGZCDbHua", // Betelgeuse Live
-    "https://www.youtube.com/embed/xRPjKQtRXR8?si=68MC_3YOs0Q5oI4S", // ISS Live Stream afarTV
-    "https://www.youtube.com/embed/VdFK-xs_r-4?si=iZ9kjNz4VLUqsRi-", // NASA ISS LIVE
-    "https://www.youtube.com/embed/DfEr5XCFNWM?si=IvEgBSoJaYWLxnAW", // NASA ISS VIEWS
-    "https://www.youtube.com/embed/O9mYwRlucZY?si=XUq7wTUFINpDVpx6", // NASA TV'S MEDIA
-    "https://www.youtube.com/embed/DnVnpyqIbsc?si=t5SMjiBwUHskN-k2", // NASA TV
-  ],
+  // config2: [
+  //   "https://www.youtube.com/embed/cOmmvhDQ2HM?si=sxVgPt6NgoFhO_78", // McGregor NSF
+  //   "https://www.youtube.com/embed/Jm8wRjD3xVA?si=Pdu_UE9vCTLT6Weq", // Space Coast NSF
+  //   "https://www.youtube.com/embed/uH1W01t_lGc?si=8vw_TLcuj2ojPLyn", // Cape Cam KSC LabPadre
+  //   "https://www.youtube.com/embed/FTw5xuq2swo?si=74Iod0bXsfevK9JC", // SpaceX port Cape Canaveral LabPadre
+  //   "https://www.youtube.com/embed/GN_j_kVsbpI?si=4cJiMh-sjA3RoRGZ", // James Web Live Data
+  //   "https://www.youtube.com/embed/gMLwtXCfo_w?si=li_JcfcEw13MlUIr", // Starman Live Data
+  //   "https://www.youtube.com/embed/bQxEcdz_g8g?si=hLafO12LGZCDbHua", // Betelgeuse Live
+  //   "https://www.youtube.com/embed/xRPjKQtRXR8?si=68MC_3YOs0Q5oI4S", // ISS Live Stream afarTV
+  //   "https://www.youtube.com/embed/VdFK-xs_r-4?si=iZ9kjNz4VLUqsRi-", // NASA ISS LIVE
+  //   "https://www.youtube.com/embed/DfEr5XCFNWM?si=IvEgBSoJaYWLxnAW", // NASA ISS VIEWS
+  //   "https://www.youtube.com/embed/jKHvbJe9c_Y?si=wzmnq-D1A139l47V", // NASA ISS
+  //   "https://www.youtube.com/embed/H999s0P1Er0?si=FXz5P_rjHflof8a2", // ISS LIVE VIEWS
+  // ],
 };
 
 function createIframes(videoUrls) {
